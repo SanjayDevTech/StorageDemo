7f9f15a6e08b62f485d6b702dee4a9b0ff9658f2
SanjayDevTech <sureshsanjay805@gmail.com>
2022-06-25 19:18:30 +0530

    Added M3, Internal storage

--- app/src/main/java/com/example/storagedemo/MainActivity.kt
+++ app/src/main/java/com/example/storagedemo/MainActivity.kt
@@ -1,43 +1,109 @@
 package com.example.storagedemo
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.storagedemo.ui.theme.StorageDemoTheme
+import java.io.File
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             StorageDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                MainScreen()
             }
         }
     }
 }
 
+fun Context.writeToFile(filename: String, contents: String) {
+    val file = File(applicationContext.filesDir, filename)
+    file.writeText(contents)
+}
+
+fun Context.readFromFile(filename: String): String {
+    val file = File(applicationContext.filesDir, filename)
+    return file.readText()
+}
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainScreen() {
+    var contents by remember {
+        mutableStateOf(
+            """
+            Hello
+            Contents of the file
+        """.trimIndent()
+        )
+    }
+    var filename by remember {
+        mutableStateOf("hello.txt")
+    }
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+    ) {
+        TextField(
+            value = contents,
+            onValueChange = { contents = it },
+            label = { Text(text = "Contents") },
+            placeholder = {
+                Text(text = "Type the contents")
+            },
+        )
+        TextField(
+            value = filename,
+            onValueChange = { filename = it },
+            singleLine = true,
+            label = { Text(text = "Filename") },
+            placeholder = {
+                Text(text = "Type the filename")
+            },
+        )
+        Button(onClick = {
+            try {
+                context.writeToFile(filename, contents)
+                Toast.makeText(context, "Written to file $filename", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Toast.makeText(context, "Exception: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+        }) {
+            Text(text = "Write to file")
+        }
+        Button(onClick = {
+            contents = ""
+            try {
+                contents = context.readFromFile(filename)
+                Toast.makeText(context, "Read from file $filename", Toast.LENGTH_SHORT).show()
+            } catch (e: Exception) {
+                Toast.makeText(context, "Exception: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+        }) {
+            Text(text = "Read from file")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    StorageDemoTheme(darkTheme = true) {
-        Greeting("Android")
+fun MainScreenPreview() {
+    StorageDemoTheme {
+        MainScreen()
     }
 }
--- app/src/main/java/com/example/storagedemo/ui/theme/Theme.kt
+++ app/src/main/java/com/example/storagedemo/ui/theme/Theme.kt
@@ -1,21 +1,21 @@
 package com.example.storagedemo.ui.theme
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.darkColors
-import androidx.compose.material.lightColors
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorPalette = darkColors(
+private val DarkColorScheme = darkColorScheme(
     primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Purple700,
+    tertiary = Teal200
 )
 
-private val LightColorPalette = lightColors(
+private val LightColorScheme = lightColorScheme(
     primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    secondary = Purple700,
+    tertiary = Teal200
 
     /* Other default colors to override
     background = Color.White,
@@ -29,14 +29,16 @@ private val LightColorPalette = lightColors(
 
 @Composable
 fun StorageDemoTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colorScheme = when {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
     }
 
     MaterialTheme(
-        colors = colors,
+        colorScheme = colorScheme,
         typography = Typography,
         shapes = Shapes,
         content = content
