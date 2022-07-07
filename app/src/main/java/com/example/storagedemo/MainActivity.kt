6f18eda26339f9900d50755c3b52004de8f90c86
SanjayDevTech <sureshsanjay805@gmail.com>
2022-07-07 10:33:22 +0530

    Added List files page

--- app/src/main/java/com/example/storagedemo/MainActivity.kt
+++ app/src/main/java/com/example/storagedemo/MainActivity.kt
@@ -1,19 +1,43 @@
 package com.example.storagedemo
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.Surface
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.storagedemo.ui.theme.StorageDemoTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val ctx = LocalContext.current
             StorageDemoTheme {
-                Surface {
+                Scaffold(
+                    topBar = {
+                        SmallTopAppBar(
+                            title = {
+                                Text(text = "Storage Demo")
+                            },
+                            actions = {
+                                IconButton(onClick = {
+                                    val intent = Intent(ctx, ListActivity::class.java)
+                                    intent.putExtra("file_path", "")
+                                    ctx.startActivity(intent)
+                                }) {
+                                    Icon(painter = painterResource(id = R.drawable.ic_baseline_view_list_24), contentDescription = "List Files")
+                                }
+                            }
+                        )
+                    }
+                ) {
                     MainScreen()
                 }
             }
