c9155dc73e20abf737de377762739afec5f2fb2c
SanjayDevTech <sureshsanjay805@gmail.com>
2022-07-07 11:06:38 +0530

    Added List files page

--- app/src/main/java/com/example/storagedemo/ListActivity.kt
+++ app/src/main/java/com/example/storagedemo/ListActivity.kt
@@ -4,11 +4,14 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -18,7 +21,6 @@ import com.example.storagedemo.ui.theme.StorageDemoTheme
 import java.io.File
 
 class ListActivity : ComponentActivity() {
-    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,25 +30,25 @@ class ListActivity : ComponentActivity() {
         }
         setContent {
             StorageDemoTheme {
-                var currentFilePair: Pair<String, File> by remember {
-                    mutableStateOf(filePath to File(filePath))
+                var currentFile by remember {
+                    mutableStateOf(File(filePath))
                 }
                 val files by remember {
-                    derivedStateOf { currentFilePair.second.listFiles() ?: emptyArray() }
+                    derivedStateOf { currentFile.listFiles() ?: emptyArray() }
                 }
 
                 Scaffold(
                     topBar = {
                         SmallTopAppBar(
                             title = {
-                                Text(text = currentFilePair.first)
+                                Text(text = "List Files")
                             }, navigationIcon = {
                                 IconButton(onClick = {
-                                    if (currentFilePair.first == filePath) {
+                                    if (currentFile.path == filePath) {
                                         onBackPressed()
                                     } else {
-                                        val parentFile = currentFilePair.second.parentFile!!
-                                        currentFilePair = parentFile.absolutePath to parentFile
+                                        val parentFile = currentFile.parentFile!!
+                                        currentFile = parentFile
                                     }
                                 }) {
                                     Icon(
@@ -56,13 +58,21 @@ class ListActivity : ComponentActivity() {
                                 }
                             })
                     }
-                ) {
-                    LazyColumn {
-                        items(files) { file ->
+                ) { paddingValues ->
+                    LazyColumn(modifier = Modifier.padding(paddingValues)) {
+                        item {
+                            Row(modifier = Modifier.horizontalScroll(rememberScrollState()).padding(4.dp)) {
+                                Text(text = currentFile.path)
+                            }
+                        }
+                        items(files, key = {it.path}) { file ->
                             Row(
                                 modifier = Modifier
                                     .fillMaxWidth()
-                                    .padding(8.dp),
+                                    .clickable {
+                                        if (file.isDirectory) currentFile = file
+                                    }
+                                    .padding(12.dp),
                             ) {
                                 Icon(
                                     painter = painterResource(id = if (file.isFile) R.drawable.ic_file else R.drawable.ic_folder),
